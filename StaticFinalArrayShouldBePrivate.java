d7ec5334794f5fcc2a1cd4ddfd1c084af86f6644
ShantanuNimbalkar <87299589+ShantanuNimbalkar@users.noreply.github.com>
2022-04-06 16:22:11 +0530

    Add files via upload

--- StaticFinalArrayShouldBePrivate.java
+++ StaticFinalArrayShouldBePrivate.java
@@ -13,15 +13,26 @@ public class StaticFinalArrayShouldBePrivate{
     // EMB-ISSUE: CodeIssueNames.STATIC_FINAL_ARRAY_SHOULD_BE_PRIVATE
     public static final String[][] arr7;
 
-    // EMB-ISSUE: CodeIssueNames.STATIC_FINAL_ARRAY_SHOULD_BE_PRIVATE/no-detect
+	public static final int[] arr10={1,2,3};    
+
     private static final int[] arr1={1,2,3};
 
     // EMB-ISSUE: CodeIssueNames.STATIC_FINAL_ARRAY_SHOULD_BE_PRIVATE
-    public static final int[] arr5=arr1;
+    private static final int[] arr5=arr1;
 
     // EMB-ISSUE: CodeIssueNames.STATIC_FINAL_ARRAY_SHOULD_BE_PRIVATE/no-detect
-    private static final int[] arr9=arr1;
+    public static final int[] arr9=arr1;
 
     // EMB-ISSUE: CodeIssueNames.STATIC_FINAL_ARRAY_SHOULD_BE_PRIVATE/no-detect
     private static final String array[]={"1","2","3"};
+
+
+`	
+    static final SecureRandom secureRandom = new SecureRandom();
+
+	private static final SecureRandom secureRN = new SecureRandom();
+    public int GenerateRandomValue(){
+        return secureRandom.nextInt();
+    }
+
 }
