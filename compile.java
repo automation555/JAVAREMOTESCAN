e225f734bdb24f4427f0b8cd07e5c4969ff713fb
ShantanuNimbalkar <87299589+ShantanuNimbalkar@users.noreply.github.com>
2022-04-06 17:08:31 +0530

    Update compile.java

--- compile.java
+++ compile.java
@@ -1,7 +1,6 @@
 public class compile{
 	static final SecureRandom secureRandom = new SecureRandom();
 
-	 static final SecureRandom secureRN = new SecureRandom();
 	 int GenerateRandomValue(){
         return secureRandom.nextInt();
     }
