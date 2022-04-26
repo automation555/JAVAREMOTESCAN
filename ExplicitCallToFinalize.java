0cda4d7f48a0278b782443c92b23b7619d42d41d
ShantanuNimbalkar <shantanu.nimbalkar@embold.io>
2022-04-26 11:31:52 +0530

    tr2 embold --skip [Embold Gated commit disabled]
    *Current Corona Version*

---
+++ ExplicitCallToFinalize.java
@@ -0,0 +1,18 @@
+package javacodechecker;
+
+public class ExplicitCallToFinalize {
+    public void methodCall(){
+
+        // EMB-ISSUE: CodeIssueNames.EXPLICIT_CALL_TO_FINALIZE
+        object1.finalize();
+
+        // EMB-ISSUE: CodeIssueNames.EXPLICIT_CALL_TO_FINALIZE/no-detect
+        object2 = null;
+	object2.finalize();
+        // EMB-ISSUE: CodeIssueNames.EXPLICIT_CALL_TO_FINALIZE/no-detect
+        object1.methodCall();
+
+
+
+    }
+}
