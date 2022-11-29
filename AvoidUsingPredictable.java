feaa299c6d8afe4b4f621a8440e920951b9d945e
ShantanuNimbalkar <87299589+ShantanuNimbalkar@users.noreply.github.com>
2022-11-29 17:55:38 +0530

    TES-8: added new issue

--- AvoidUsingPredictable.java
+++ AvoidUsingPredictable.java
@@ -8,6 +8,7 @@ public class AvoidUsingPredictable {
         // EMB-ISSUE: CodeIssueNames.AVOID_USING_PREDICTABLE_RANDOM_VALUES
         Random r = new Random();
         return Long.toHexString(r.nextLong());
+        System.out.println("added new issue");
 
         // EMB-ISSUE: CodeIssueNames.AVOID_USING_PREDICTABLE_RANDOM_VALUES/no-detect
         SecureRandom secureRandom = new SecureRandom();
