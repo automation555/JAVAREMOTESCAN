99eb201e168b517efaa161bd189e6e8d1821fdd2
ShantanuNimbalkar <87299589+ShantanuNimbalkar@users.noreply.github.com>
2022-11-30 13:12:36 +0530

    TEST-7: added sysout

--- compile.java
+++ compile.java
@@ -2,6 +2,27 @@ public class compile{
 	static final SecureRandom secureRandom = new SecureRandom();
 
 	 int GenerateRandomValue(){
+		 System.out.println("added new code issues");
+		  System.out.println("added new code issues");
+		  System.out.println("added new code issues");
+		  System.out.println("added new code issues");
+		  System.out.println("added new code issues");
+		  System.out.println("added new code issues");
+		  System.out.println("added new code issues");
+		 System.out.println("added new code issues");
+		  System.out.println("added new code issues");
+		  System.out.println("added new code issues");
+		  System.out.println("added new code issues");
+		  System.out.println("added new code issues");
+		  System.out.println("added new code issues");
+		  System.out.println("added new code issues");
+		 System.out.println("added new code issues");
+		  System.out.println("added new code issues");
+		  System.out.println("added new code issues");
+		  System.out.println("added new code issues");
+		  System.out.println("added new code issues");
+		  System.out.println("added new code issues");
+		  System.out.println("added new code issues");
         return secureRandom.nextInt();
     }
 }
