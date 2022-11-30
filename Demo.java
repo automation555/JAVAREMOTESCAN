07bdaea4158bc2bf6ca30d6cf6a233c7084169ea
ShantanuNimbalkar <87299589+ShantanuNimbalkar@users.noreply.github.com>
2022-11-30 13:10:51 +0530

    TEST-7: added new issue

--- Demo.java
+++ Demo.java
@@ -10,6 +10,12 @@ public class Demo {
         return Long.toHexString(r.nextLong());
 
         // EMB-ISSUE: CodeIssueNames.AVOID_USING_PREDICTABLE_RANDOM_VALUES/no-detect
-        SecureRandom secureRandom = new SecureRandom();
+        SecureRandom secureRandom = new SecureRandom
+             object4.finalize();
+	     object5.finalize();
+	     object6.finalize();
+	     object7.finalize();
+	     object8.finalize();
+	     object9.finalize();
     }
 }
