f8987f30a005747e2a2152ef6fa2264241d65563
ShantanuNimbalkar <87299589+ShantanuNimbalkar@users.noreply.github.com>
2022-12-13 19:20:38 +0530

    TEST-9: added changes

--- AApplicationConstant.java
+++ AApplicationConstant.java
@@ -6,15 +6,13 @@ import java.util.List;
 import org.springframework.stereotype.Component;
 
 @Component
-private class AApplicationConstant {
+private final class AApplicationConstant {
 
 	
 	public static final List<String> WHITELIST_URL = new ArrayList(Arrays.asList("/oauth/grant", "/oauth/grant/login", "/oauth/revoke/",
 			"/keycloak/grant", "/oauth/changepassword", "/saveRephraseCallBackData", "/info", "/configdata", "health/grantAuthentication",
 			"/fleetnames", "/getFleetData", "/save/preinspection", "/getMappedFieldNames", "/activitystatus", "/sheetname", "/health/","/region","/check","/validate/otp","/generate/otp" ));
 
-	private ApplicationConstant() {
-
-	}
+	
 
 }
