d0c57f1a4ecab8e738a3a4261f3af7679ec4d86b
ShantanuNimbalkar <87299589+ShantanuNimbalkar@users.noreply.github.com>
2022-12-13 19:38:02 +0530

    TEST-19: added codeissue

---
+++ ABApplicationConstant.java
@@ -0,0 +1,17 @@
+package com.digitplus.constants;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.stereotype.Component;
+
+@Component
+private final class ABApplicationConstant {
+
+	
+	public static final List<String> WHITELIST_URL = new ArrayList(Arrays.asList("/oauth/grant", "/oauth/grant/login", "/oauth/revoke/",
+			"/keycloak/grant", "/oauth/changepassword", "/saveRephraseCallBackData", "/info", "/configdata", "health/grantAuthentication",
+			"/fleetnames", "/getFleetData", "/save/preinspection", "/getMappedFieldNames", "/activitystatus", "/sheetname", "/health/","/region","/check","/validate/otp","/generate/otp" ));
+
+	
+
