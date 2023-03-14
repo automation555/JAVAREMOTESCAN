c82a2fc14df70ceb1ba41f3a3de0df5bd275a090
rajnikant <59799061+automation555@users.noreply.github.com>
2023-03-14 21:44:51 +0530

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,106 @@
+package javacodechecker;
+
+public class Demo {
+    public void bad() throws Throwable
+    {
+        // EMB-ISSUE: CodeIssueNames.RETURN_IN_FINALLY_BLOCK
+        try
+        {
+            throw new IllegalArgumentException();
+        }
+        catch(IllegalArgumentException exceptIllegalArgument)
+        {
+            IO.writeLine("preventing incidental issues");
+        }
+        finally
+        {
+            if(true)
+            {
+                return;
+               
+            }
+        }
+
+    }
+
+    public void bad1() throws Throwable
+    {
+        // EMB-ISSUE: CodeIssueNames.RETURN_IN_FINALLY_BLOCK
+        try
+        {
+            throw new IllegalArgumentException();
+        }
+        catch(IllegalArgumentException exceptIllegalArgument)
+        {
+            IO.writeLine("preventing incidental issues");
+        }
+        finally
+        {
+                return hello;
+            
+              System.out.println("code issue added");   
+                    
+        }
+    }
+
+
+    private void good1() throws Throwable
+    {
+        // EMB-ISSUE: CodeIssueNames.RETURN_IN_FINALLY_BLOCK/no-detect
+        try
+        {
+            throw new IllegalArgumentException();
+        }
+        catch(IllegalArgumentException exceptIllegalArgument)
+        {
+            IO.writeLine("preventing incidental issues");
+        }
+        finally
+        {
+            IO.writeLine("In finally block, cleaning up");
+        }
+
+    }
+
+    private void good2() throws Throwable
+    {
+        // EMB-ISSUE: CodeIssueNames.RETURN_IN_FINALLY_BLOCK/no-detect
+        try
+        {
+            throw new IllegalArgumentException();
+        }
+        catch(IllegalArgumentException exceptIllegalArgument)
+        {
+            IO.writeLine("preventing incidental issues");
+        }
+        finally
+        {
+            IO.writeLine("In finally block, cleaning up");
+            return;
+        }
+
+    }
+    public void good3() throws Throwable
+    {
+        // EMB-ISSUE: CodeIssueNames.RETURN_IN_FINALLY_BLOCK/no-detect
+        try
+        {
+            throw new IllegalArgumentException();
+        }
+        catch(IllegalArgumentException exceptIllegalArgument)
+        {
+            IO.writeLine("preventing incidental issues");
+        }
+        finally
+        {
+            if(true)
+            {
+             
+                return;
+
+            }
+        }
+
+    }
+
+}
